91a95e9f60c9a2eac4260dbef8e1504a6693465d
Your Name <you@example.com>
2024-03-26 16:53:11 +0700

    feat: register, login

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, Outlet, useLocation} from 'react-router-dom';
 //import pages
 import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails'
 import ProductType from './pages/ProductType';
+import Login from './pages/login';
+import Register from './pages/register';
 //import components
 import Sidebar from './components/Sidebar'
 import Header from './components/Header'
+import LoginHeader from './components/LoginHeader';
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
+
+
+
 const App = () => {
-  return <div className='overflow-hidden'>
-    <Router>
-      <Header />
-      <div className="mt-20"> {/* Add top margin to create space for the header */}
-        <Routes>
-          <Route path='/' element={<Home /> } />
-          <Route path='/product/:id' element={<ProductDetails />} />
-          <Route path='/:p_type/:type' element={<ProductType />} />
-          {/* <Route path='/womenClothing/:type' element={<WomenClothing />} /> */}
-        </Routes>
-      </div>
-      <Sidebar />
-      <Footer />
-    </Router>
-  </div>
+
+  const isLoginPage = () => {
+    return window.location.pathname === '/login' || window.location.pathname === '/register';
+  };
+
+  return (
+    <div className='overflow-hidden'>
+      {isLoginPage() ?
+      <Router>
+          <Routes>
+            <Route path='/login' element= {<Login />} />
+            <Route path = '/register' element = {<Register/>} />
+          </Routes>
+      </Router>
+      :
+      <Router>
+        <Header />
+        <div className="mt-20"> {/* Add top margin to create space for the header */}
+          <Routes>
+            <Route path='/' element={<Home /> } />
+            <Route path='/product/:id' element={<ProductDetails />} />
+            <Route path='/:p_type/:type' element={<ProductType />} />
+          </Routes>
+        </div>
+        <Sidebar />
+        <Footer />
+      </Router>
+      }
+    </div>
+  )
 }
 
 export default App;
