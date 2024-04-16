cd2d855aef77d51eac5249a7ee17182e571cb8cf
Your Name <you@example.com>
2024-04-16 15:09:18 +0700

    merge test 2 16/04/2024

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useContext} from 'react'
 //import product context
 import { ProductContext } from '../context/ProductContext'
@@ -30,49 +29,4 @@ const Home = () => {
   );
 }
 
-=======
-import React, {useContext} from 'react'
-//import product context
-import { ProductContext } from '../context/ProductContext'
-//import components
-import Product from '../components/Product'
-//import axios
-import axios from 'axios';
-//import Link
-import { Link } from 'react-router-dom';
-const Home = () => {
-  // get products from product context
-  const { products } = useContext(ProductContext);
-  const isLoggedIn = localStorage.getItem('token') !== null;
-  //filtered category
-  // const filteredProducts = products.filter((item) => {
-  //   return (
-  //     item.category === "men's clothing" || item.category === "women's clothing"
-  //   );
-  // });
-  return (
-    <div>
-      {isLoggedIn ? (
-        // Content to display if the user is logged in
-        <section className='py-20'>
-          <div className='container mx-auto p-12'>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
-              {products.map(product => (
-                <Product product={product} key={product.id} />
-              ))}
-            </div>
-          </div>
-        </section>
-      ) : (
-        // Content to display if the user is not logged in
-        <div className="text-center py-20">
-          <h2 className="text-2xl font-semibold mb-4">Please log in to view products</h2>
-          <Link to="/login" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Log In</Link>
-        </div>
-      )}
-    </div>
-  );
-}
-
->>>>>>> test
 export default Home
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-import React, { useContext } from 'react'
-//import useParams
-import {useParams} from 'react-router-dom'
-//import cart context
-import { CartContext } from '../context/CartContext'
-//import product context
-import { ProductContext } from '../context/ProductContext'
-//Comment
-import Comments from '../components/Comment/Comments'
-
-const ProductDetails = () => {
-  //get the product id from the url
-  const {id} = useParams();
-  const { products } = useContext(ProductContext)
-  const { addToCart } = useContext(CartContext)
-  //get the single product based on id
-  const product = products.find(item => {
-    return item.id === parseInt(id)
-  })
-
-  //if product is not found
-  if (!product) {
-    return <section className='h-screen flex justify-center items-center'>
-      Loading...
-    </section>
-  }
-  //destructure product
-  const {title, price, description, image} = product
-  return (
-    <section className='mt-4 pt-20 pb-12 lg-py-25 h-auto'>
-      <div className='container mx-auto'>
-        {/* image & text wrapper */}
-        <div className='flex flex-col lg:flex-row lg:flex-row-reverse items-center'>
-          {/* text */}
-          <div className='flex-1 text-center lg:text-left'>
-            <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto'>
-              {title}
-            </h1>
-            <div className='mb-6 flex justify-center lg:justify-start'>  
-              <p className='text-xl text-red-500 font-medium inline-block'>
-                $ {price}
-              </p>
-            </div>
-            <p className='mb-8'>{description}</p>
-            <div className='flex justify-center lg:justify-start'></div>
-              <button onClick={() => addToCart(product, product.id)} className='bg-stone-950 py-4 px-8 text-white'>Thêm vào giỏ hàng</button>
-            </div>
-          {/* image */}
-          <div className='flex flex-1 justify-center items-center mb-8 lg:mb-0'>
-            <img className='max-w-[200px] lg:max-w-sm' src={image} alt='' />
-          </div>
-        </div>
-        <Comments />
-      </div>
-      
-    </section>
-  )
-}
-
-=======
 import React, { useContext } from 'react'
 //import useParams
 import {useParams} from 'react-router-dom'
@@ -117,5 +56,4 @@ const ProductDetails = () => {
   )
 }
 
->>>>>>> test
 export default ProductDetails
