3bc101d878e121a57537897b8cb0dfe86542ab59
Casro <118825614+Dat0937947595@users.noreply.github.com>
2024-04-23 17:11:34 +0700

    update general info

--- server/routes/laptop/index.js
+++ server/routes/laptop/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const laptopRouter = express.Router();
 const commentsRoute = require("./comments.route.js");
+const general_infoRoute = require("./general_info.js");
 const spdRoute = require("./specific_data.js");
 
 //Default
@@ -11,6 +12,7 @@ laptopRouter.get("/", (req, res) => {
 
 //Routes
 // phoneRouter.use("/general_info", genral_infoRoute);
+laptopRouter.use("/general_info", general_infoRoute);
 laptopRouter.use("/comments", commentsRoute);
 laptopRouter.use("/specific_data", spdRoute);
 module.exports = laptopRouter;
